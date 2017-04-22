084b9083fcc75407b295e963c882f2653ff59166
GinMu <mu.sheng@moxtra.com>
2017-04-22 14:58:51 +0800

    added ignore string

--- grepRecord.js
+++ grepRecord.js
@@ -25,13 +25,39 @@ const rl = readline.createInterface({
     terminal: false
 });
 
+
+const ignoreIcons = [
+    'micon-shape-rectangle',
+    'micon-shape-triangle',
+    'micon-shape-ellipse',
+    'micon-shape-line',
+    'micon-network-disabled',
+    'micon-network-medium',
+    'micon-network-strong',
+    'micon-network-weak',
+    'micon-message'
+];
+
+const ignoreString = ignoreIcons.join(',');
+
+const isExist = (line) => {
+  return ignoreString.includes(line.trim());
+}
+
 rl.on('line', (line) => {
-    excuteShell(directory, line);
-})
+    rl.pause();
+
+    if (isExist(line)) {
+      let data = line + '动态生成';
+      saveRecord(data);
+    } else {
+      excuteShell(directory, line);
+    }
+});
+
 
 
 const excuteShell = (directory, line) => {
-    rl.pause();
     let statisticsCommand = getCommand(directory, line);
     child_process.exec(statisticsCommand, {
         encoding: 'utf8'
@@ -42,14 +68,18 @@ const excuteShell = (directory, line) => {
             return;
         }
         let data = line + '出现:' + output.trim() + '次';
-        let existed = fs.existsSync(savefile);
-        if (existed) {
-            fs.appendFileSync(savefile, data + '\n');
-        } else {
-            fs.writeFileSync(savefile, data + '\n');
-        }
-        rl.resume();
+        saveRecord(data);
     });
+}
+
+const saveRecord = (data) => {
+    let existed = fs.existsSync(savefile);
+    if (existed) {
+        fs.appendFileSync(savefile, data + '\n');
+    } else {
+        fs.writeFileSync(savefile, data + '\n');
+    }
+    rl.resume();
 }
 
 const getCommand = (directory, line) => {
